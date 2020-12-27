665fe1c7363494f0b2395d5eba24fc728605b5a1
rafaelandrade <rafasouza@protonmail.com>
2020-12-27 18:33:58 -0300

    Update

---
+++ utils/config_chrome_driver.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+
+
+def creation_of_instance():
+	"""Function responsible for creation of chrome driver instance"""
+	options = webdriver.ChromeOptions()
+
+	options.add_argument("--headless")
+	options.add_argument("--no-sandbox")
+	options.add_argument("--disable-dev-sh-usage")
+	options.add_argument("--window-size=1920,1080")
+	options.add_argument("--disable-gpu")
+	options.add_argument("--start-maximized")
+	options.add_argument('--lang=en')
+	options.add_experimental_option("excludeSwitches", ["enable-automation"])
+	options.add_experimental_option('useAutomationExtension', False)
+	options.add_argument(
+		"user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) "
+		"Chrome/71.0.3578.98 Safari/537.36")
+
+	return options
